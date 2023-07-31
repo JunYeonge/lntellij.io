0fb6dc812ce7742928594b55ab4c2dd5907feb9b
junyeonge <ehdrjs1112@naver.com>
2023-07-31 11:09:51 +0900

    0731

---
+++ ipashop/src/main/java/jpabook/ipashop/IpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.ipashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpashopApplication.class, args);
+	}
+
+}
---
+++ ipashop/src/test/java/jpabook/ipashop/IpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.ipashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
