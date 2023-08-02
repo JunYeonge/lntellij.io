538f24a3d53cbaa955a6bf6e057f5d6f1765a4cb
junyeonge <ehdrjs1112@naver.com>
2023-08-02 17:46:24 +0900

    0802

--- ipashop/src/main/java/jpabook/jpashop/controller/HelloController.java
+++ ipashop/src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
 
     @GetMapping("hello")
     public String hello(Model model){
