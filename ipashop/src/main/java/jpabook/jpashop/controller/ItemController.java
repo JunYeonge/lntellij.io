3412b49dbd738a5cf8d875adb5c241a67fb0effe
junyeonge <ehdrjs1112@naver.com>
2023-08-03 17:13:36 +0900

    0803

--- ipashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ ipashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -40,4 +42,48 @@ public class ItemController {
         model.addAttribute("items", items);
         return "items/itemList";
     }
-}
+
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Book item = (Book) itemService.findOne(itemId);
+        BookForm form = new BookForm();
+
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form",form);
+        return "items/updateItemForm";
+    }
+
+    // 1. itemService는 아이템과 관련된 비즈니직을 처리하는 클래스
+    // findOne(itemId) 메소드 호출해서 itemId 해당하는 아이템을 조회
+    // 조회된 아이템을 Book타입으로 캐스팅
+
+    // 2. 수정 폼에 렌더링할 'BookForm' 객체를 생성합니다.
+    //BookForm은 아이템 정보를 표시하고 수정하기 위한
+    // DTO(data Transfer Object) 입니다.
+
+//    @PostMapping(value = "/items/{itemId}/edit")
+//    public String updateItem(@ModelAttribute("form") BookForm form) {
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
+//        return "redirect:/items";
+//    }
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form){
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+        return "redirect:/items";
+    }
+
+    }
+
