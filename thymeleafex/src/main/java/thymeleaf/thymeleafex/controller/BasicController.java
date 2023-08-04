c7ea03555ff2d1fd2f741fc8b17db55c7e5aa55b
junyeonge <ehdrjs1112@naver.com>
2023-08-04 19:25:38 +0900

    0804

--- thymeleafex/src/main/java/thymeleaf/thymeleafex/controller/BasicController.java
+++ thymeleafex/src/main/java/thymeleaf/thymeleafex/controller/BasicController.java
@@ -1,11 +1,13 @@
 package thymeleaf.thymeleafex.controller;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @Controller
@@ -36,6 +38,68 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObject(HttpSession httpSession) {
+        httpSession.setAttribute("sessionData","Hello Session");
+        return "basic/basic-object";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String liteeal(Model model) {
+        model.addAttribute("data","Spring");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData",null);
+        model.addAttribute("data","Spring");
+        return "basic/operation";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> users = Arrays.asList(new User("userA",10),
+                new User("userB",20),
+                new User("userC",30));
+
+        model.addAttribute("users",users);
+
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers1(model);
+        return "basic/condition";
+    }
+
+    private void addUsers1(Model model) {
+        List<User> users = Arrays.asList(new User("userA",10),
+                new User("userB",20),
+                new User("userC",30));
+
+        model.addAttribute("users",users);
     }
 
 
